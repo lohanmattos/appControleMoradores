7ec407dcda52f365c086d17b389d033a8e61e5ab
Lohan <lohanamendola18@gmail.com>
2024-11-24 16:04:22 -0400

    add o template admin

--- src/main/java/dev/amendola/appControleMoradores/Controler/InicioControler.java
+++ src/main/java/dev/amendola/appControleMoradores/Controler/InicioControler.java
@@ -9,10 +9,8 @@ public class InicioControler {
 
     @GetMapping("/")
     public String home(Model model) {
-        // Título e Descrição
-        model.addAttribute("titulo", "CorujaCondo - Controle de Condomínio");
+        model.addAttribute("titulo", "Página Inicial");
         model.addAttribute("descricao", "Sistema para gerenciar a vila Militar de forma eficiente.");
-
         return "inicio";
     }
 }
