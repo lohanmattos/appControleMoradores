aa871e6fdbd53624a689b920051e7c8b1ce0d07d
Lohan <lohanamendola18@gmail.com>
2024-11-26 15:02:42 -0300

    editando as categorias de contas no html

--- src/main/java/dev/amendola/appControleMoradores/Controler/DespesaController.java
+++ src/main/java/dev/amendola/appControleMoradores/Controler/DespesaController.java
@@ -1,7 +1,14 @@
 package dev.amendola.appControleMoradores.Controler;
 
+import dev.amendola.appControleMoradores.Model.CategoriaContas;
 import dev.amendola.appControleMoradores.Model.Despesa;
+import dev.amendola.appControleMoradores.Model.TipoCategoria;
+import dev.amendola.appControleMoradores.Service.CategoriaContasService;
 import dev.amendola.appControleMoradores.Service.DespesaService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +19,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class DespesaController {
 
     private final DespesaService despesaService;
+    
+    @Autowired
+    private CategoriaContasService categoriaContasService;
 
     public DespesaController(DespesaService despesaService) {
         this.despesaService = despesaService;
@@ -21,6 +31,10 @@ public class DespesaController {
     public String listarDespesas(Model model) {
         model.addAttribute("despesas", despesaService.listarTodas());
         model.addAttribute("despesa", new Despesa()); // Para o modal
+        
+        List<CategoriaContas> categoriasDespesas = categoriaContasService.listarPorTipo(TipoCategoria.DESPESA);
+
+        model.addAttribute("categoriasDespesas", categoriasDespesas);
         return "financeiro/despesas/lista";
     }
 
--- src/main/java/dev/amendola/appControleMoradores/Controler/ReceitaController.java
+++ src/main/java/dev/amendola/appControleMoradores/Controler/ReceitaController.java
@@ -1,21 +1,30 @@
 package dev.amendola.appControleMoradores.Controler;
 
 import dev.amendola.appControleMoradores.Model.Receita;
+import dev.amendola.appControleMoradores.Model.TipoCategoria;
+import dev.amendola.appControleMoradores.Service.CategoriaContasService;
 import dev.amendola.appControleMoradores.Service.ReceitaService;
 
 import java.text.NumberFormat;
+import java.util.List;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import dev.amendola.appControleMoradores.Model.CategoriaContas;
 
 @Controller
 @RequestMapping("/receitas")
 public class ReceitaController {
 
     private final ReceitaService receitaService;
+    
+    @Autowired
+    private CategoriaContasService categoriaContasService;
 
     public ReceitaController(ReceitaService receitaService) {
         this.receitaService = receitaService;
@@ -28,6 +37,10 @@ public class ReceitaController {
         model.addAttribute("receitas", receitaService.listarTodas());
         model.addAttribute("receita", new Receita()); // Inicializa o objeto para o modal
 
+        List<CategoriaContas> categoriasReceitas = categoriaContasService.listarPorTipo(TipoCategoria.RECEITA);
+
+        model.addAttribute("categoriasReceitas", categoriasReceitas);
+        
         return "financeiro/receitas/lista"; // Redireciona para o template receitas/lista.html
     }
 
