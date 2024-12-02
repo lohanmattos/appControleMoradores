5661bd35ea731f864e6dfb39c9c5d887721346f5
Lohan <lohanamendola18@gmail.com>
2024-12-02 18:57:01 -0300

    editando os dados dos usuarios responsaveis - codificando a senha

--- src/main/java/dev/amendola/appControleMoradores/Service/UsuarioResponsavelService.java
+++ src/main/java/dev/amendola/appControleMoradores/Service/UsuarioResponsavelService.java
@@ -1,8 +1,11 @@
 package dev.amendola.appControleMoradores.Service;
 
+import dev.amendola.appControleMoradores.Model.Perfil;
 import dev.amendola.appControleMoradores.Model.UsuarioResponsavel;
+import dev.amendola.appControleMoradores.Repository.PerfilRepository;
 import dev.amendola.appControleMoradores.Repository.UsuarioResponsavelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +15,12 @@ public class UsuarioResponsavelService {
 
     @Autowired
     private UsuarioResponsavelRepository responsavelRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private PerfilRepository perfilRepository;
 
     public List<UsuarioResponsavel> listarTodos() {
         return responsavelRepository.findAll();
@@ -23,6 +32,20 @@ public class UsuarioResponsavelService {
     }
 
     public void salvar(UsuarioResponsavel responsavel) {
+    	String senhaCriptografada = passwordEncoder.encode(responsavel.getUsuario().getSenha());
+    	responsavel.getUsuario().setSenha(senhaCriptografada);
+    	 // Busca o perfil padrão no banco de dados
+        Perfil perfilPadrao = perfilRepository.findByRole("ROLE_USER")
+                .orElseThrow(() -> new RuntimeException("Perfil padrão 'ROLE_USER' não encontrado."));
+        // Adiciona o perfil padrão ao usuário
+        if (responsavel.getUsuario().getPerfis() == null || responsavel.getUsuario().getPerfis().isEmpty()) {
+        	responsavel.getUsuario().setPerfis(List.of(perfilPadrao));
+        }
+        
+     // Adiciona o perfil padrão ao usuário
+        if (responsavel.getUsuario().isAtivo() == false) {
+        	responsavel.getUsuario().setAtivo(true);
+    }
         responsavelRepository.save(responsavel);
     }
 
